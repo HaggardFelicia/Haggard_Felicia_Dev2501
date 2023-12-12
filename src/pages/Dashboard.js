2223cfe3df6743cce6aef4a2bfe879deefab7007
Felicia Haggard <HaggardFelicia@outlook.com>
2023-12-11 21:35:48 -0800

    updating files, adding chart

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,13 @@
-import React from "react";
-
+import React, {useState} from "react";
+import smallChart from "../components/charts/smallChart";
 function Dashboard(){
     return(
         <div style={styles.container}>
             <h1 style={styles.h1}>Dashboard</h1>
+            const [chartSmall]=useState([
+                {chartHeader: 'chart header ex'},
+                {chartHeader: 'chart header ex'}
+            ])
         </div>
     )
 }
